5eb29afed137c809955d116e7a7764b5914add96
wycliffepepela <47030822+pepelawycliffe@users.noreply.github.com>
2021-03-07 22:14:23 +0300

    Add files via upload

---
+++ backtest.py
@@ -0,0 +1,24 @@
+#
+# Vectorized Backtesting of
+# Trading Bot (Financial Q-Learning Agent)
+#
+
+#
+import numpy as np
+import pandas as pd
+pd.set_option('mode.chained_assignment', None)
+
+def reshape(s, env):
+    return np.reshape(s, [1, env.lags, env.n_features])
+
+def backtest(agent, env):
+    done = False
+    env.data['p'] = 0
+    state = env.reset()
+    while not done:
+        action = np.argmax(
+            agent.model.predict(reshape(state, env))[0, 0])
+        position = 1 if action == 1 else -1
+        env.data.loc[:, 'p'].iloc[env.bar] = position
+        state, reward, done, info = env.step(action)
+    env.data['s'] = env.data['p'] * env.data['r']
